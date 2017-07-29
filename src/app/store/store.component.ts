be73103d9468ce8521b8cf76546b525482efac4b
John Bahamon <correo@johnbahamon.com>
2017-07-28 20:56:09 -0500

    update

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
-
 import { CartService } from "./../cart/cart.service";
+import { AppService } from './../app.service';
 import { Product } from "../model/product.model";
 
 @Component({
@@ -10,27 +9,15 @@ import { Product } from "../model/product.model";
   styleUrls: ['./store.component.css']
 })
 export class StoreComponent implements OnInit {
-  products_API:Array<Object>;
-  baseUrl = 'https://lcboapi.com/products';
-  accessKey='?access_key=MDpmNmJhMzM5ZS01ZGE5LTExZTctOTJhZC0zNzQ1YzhiOWU3OWI6Z1JkVDlENlJhaHRqMlFhVE5lRmI2MVExSVNrRE1BRWJVeExT';
+  master = 'Master';
   constructor(
-    private http:Http,
     private cart: CartService,
-
+    private appService: AppService
 	) {}
-
   ngOnInit() {
-		this.getproducts();
   }
-	//utilizamos promesas para cargar los productos
-	getproducts():void{
-		this.http.request(this.baseUrl + this.accessKey)
-			.subscribe((response:Response)=>{
-        this.products_API = response.json().result;
-			})
-  }
+
   addProductToCart(product: Product) {
 		this.cart.addLine(product);
-		//this.router.navigateByUrl("/cart");
 	}
 }
