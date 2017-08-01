c4031baef1dabaeaa10cb9687544b8c7c73176f3
John Bahamon <correo@johnbahamon.com>
2017-07-31 22:15:08 -0500

    upload img on firebase v0.1

--- src/app/products/new-product/new-product.component.ts
+++ src/app/products/new-product/new-product.component.ts
@@ -4,6 +4,10 @@ import 'rxjs/add/operator/map';
 import {NgForm } from '@angular/forms';
 import { Product } from './../../shared/product.model';
 import { ApiService } from './../../app.service';
+import { AngularFireDatabase} from 'angularfire2/database';
+import { Observable } from 'rxjs';
+import * as firebase from 'firebase';
+
 @Component({
   selector: 'app-new-product',
   templateUrl: './new-product.component.html',
@@ -13,8 +17,12 @@ export class NewProductComponent implements OnInit {
  
   loading: Boolean = false;
   newProduct: Product;
+  folder: string = 'img';
 
-  constructor(public api: ApiService) { }
+  constructor(
+    public api: ApiService,
+    public db: AngularFireDatabase, 
+  ) { }
 
   ngOnInit() {
   }
@@ -23,21 +31,37 @@ export class NewProductComponent implements OnInit {
     this.loading = true;
 
     const formValues = Object.assign({}, form.value);
+    let storageRef = firebase.storage().ref();
+        let success = false;
+        // This currently only grabs item 0, TODO refactor it to grab them all
+        for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
+            let folder = this.folder;
+            let path = `/${this.folder}/${selectedFile.name}`;
+            var iRef = storageRef.child(path);
+            iRef.put(selectedFile).then((snapshot) => {
+                //console.log('Uploaded', snapshot.totalBytes, 'bytes.');
+                //console.log(snapshot.metadata);
+                var url = snapshot.downloadURL;
+                //console.log('Uploaded a blob or file! Now storing the reference at',`/${this.folder}/`);
+                //db.list(`/${folder}/`).push({ path: path, filename: selectedFile.name })
+                //console.log('File available at', url);
+                  const product: Product = {
+                    name: `${formValues.name}`,
+                    description: `${formValues.description}`,
+                    price: formValues.price,
+                    stock: formValues.stock,
+                    picture: url
+                  };
+                  this.api.post('products', product)
+                    .subscribe(data => {
+                      form.reset();
+                      this.loading = false;
+                      this.newProduct = data;
+                    });
 
-    const product: Product = {
-      name: `${formValues.name}`,
-      description: `${formValues.description}`,
-      price: formValues.price,
-      stock: formValues.stock,
-      picture: `${formValues.picture}`
-
-    };
-
-    this.api.post('products', product)
-      .subscribe(data => {
-        form.reset();
-        this.loading = false;
-        this.newProduct = data;
-      });
+            });
+            
+        }
   }
+
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,16 +1,5 @@
 
-import { Component, OnInit, Input } from '@angular/core';
-import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import * as firebase from 'firebase';
-
-interface Image {
-    path: string;
-    filename: string;
-    downloadURL?: string;
-    $key?: string;
-}
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -19,77 +8,11 @@ interface Image {
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-    /**
-     * The name of the folder for images
-     * eg. posts/angular-is-awesome
-     */
-    @Input() folder: string;
     
-    fileList : FirebaseListObservable<Image[]>;
-    imageList : Observable<Image[]>;
-
-    constructor(public db: AngularFireDatabase, public router: Router) {
+    constructor() {
     }
     ngOnInit() {
         
-    }
-
-    ngOnChanges() {
-        console.log("new values for folder");
-        let storage = firebase.storage();
-        
-        this.fileList = this.db.list(`/${this.folder}/images`);
-        console.log("Rendering all images in ",`/${this.folder}/images`)
-        this.imageList = this.fileList.map( itemList =>
-            itemList.map( item => {
-                var pathReference = storage.ref(item.path);
-                let result = {$key: item.$key, downloadURL: pathReference.getDownloadURL(), path: item.path, filename: item.filename};
-                console.log(result);
-                return result;
-            })
-        );
-    }
-
-
-    upload() {
-        // Create a root reference
-        let storageRef = firebase.storage().ref();
-
-        let success = false;
-        // This currently only grabs item 0, TODO refactor it to grab them all
-        for (let selectedFile of [(<HTMLInputElement>document.getElementById('file')).files[0]]) {
-            console.log(selectedFile);
-            // Make local copies of services because "this" will be clobbered
-            let router = this.router;
-            let db = this.db;
-            let folder = this.folder;
-            let path = `/${this.folder}/${selectedFile.name}`;
-            var iRef = storageRef.child(path);
-            iRef.put(selectedFile).then((snapshot) => {
-                console.log('Uploaded a blob or file! Now storing the reference at',`/${this.folder}/images/`);
-                db.list(`/${folder}/images/`).push({ path: path, filename: selectedFile.name })
-            });
-        }
-        
-    }
-    delete(image: Image) {
-        let storagePath = image.path;
-        let referencePath = `${this.folder}/images/` + image.$key;
-
-        // Do these as two separate steps so you can still try delete ref if file no longer exists
-
-        // Delete from Storage
-        firebase.storage().ref().child(storagePath).delete()
-        .then(
-            () => {},
-            (error) => console.error("Error deleting stored file",storagePath)
-        );
-
-        // Delete references
-        this.db.object(referencePath).remove()
-            
-        
-
     }
 
 }
