bcea530e287627a49d719df553b3b3159ce1f6da
John Bahamon <correo@johnbahamon.com>
2017-09-06 01:09:03 -0500

    signup update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,14 @@
 import { Routes, RouterModule  } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 
-import { AuthComponent } from './auth/auth.component';
-
-
+/* import { AuthComponent } from './auth/auth.component';
 import { ChatComponent } from './chat/chat.component';
-//import { ProductsComponent } from './products/products.component';
+import { ProductsComponent } from './products/products.component';
 import { AddProductComponent } from './products/add-product/add-product.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { AddCategoryComponent } from './categories/add-category/add-category.component';
 import { OrdersComponent } from './orders/orders.component';
-
+ */
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 import { AuthRoutes } from './auth/auth.routes';
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+interface User { 
+  firstName: string; 
+  lastName: string; 
+}
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  user: User = { 
+    firstName: 'Shravan', 
+    lastName: 'Kasagoni' 
+  } 
   constructor() { }
 
   ngOnInit() {
+  }
+  onSubmit(formValue) { 
+    console.log(formValue); 
   }
 
 }
